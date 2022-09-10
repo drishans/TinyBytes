e2bb2bc985c89e3542cb6efd98c129213c9e2689
Drishan Sarkar <43171376+drishans@users.noreply.github.com>
2022-09-10 04:55:59 -0600

    Update moveObj.cs

--- Assets/_unity-housekeeping/moveObj.cs
+++ Assets/_unity-housekeeping/moveObj.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveObjExample: MonoBehaviour
+public class moveObj: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
